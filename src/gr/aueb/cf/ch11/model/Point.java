e42a97030db02b552a71766f0e8e8fc67023e719
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2024-12-23 20:29:04 +0200

    updated

---
+++ src/gr/aueb/cf/ch11/model/Point.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.ch11.model;
+
+public class Point {
+    private static int nOfPoints = 0;
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(){
+        nOfPoints;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+}
+
