cdc9dce9a2e56f9770174e2a44a9349c1895f891
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2024-12-23 15:09:03 +0200

    delete files

--- src/gr/aueb/cf/homework/AddApp.java
+++ src/gr/aueb/cf/homework/AddApp.java
@@ -1,21 +1,21 @@
 package gr.aueb.cf.homework;
-/**
- * Calculates the sum of two integers
- * and the result is printed at console
- * (standard output)
- */
+
 public class AddApp {
     public static void main(String[] args){
 
         int num1 = 5;
         int num2 = 12;
         int result = 0 ;
-        // Εντολες
+        // Εντολεςgit
         result= num1 + num2;
 
         // Εκτύπωση αποτελέσματος
         System.out.println("Το αποτέλεσμα είναι: " + result);
-        System.out.println("Το άθροισμα των " + num1 +"," + num2 + " είναι ίσο με: " + result);
-        System.out.println("To άθροισμα των %d, %d είναι ίσο με: %d/n, num1, num2, result");
+
+
+
+    }
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
