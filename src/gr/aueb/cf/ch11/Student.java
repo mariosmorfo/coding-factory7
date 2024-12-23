5daa579373492336f175e0ff9aa699a629ca8a25
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2024-12-23 20:01:16 +0200

    initial commit

--- src/gr/aueb/cf/ch11/Student.java
+++ src/gr/aueb/cf/ch11/Student.java
@@ -1,19 +1,26 @@
 package gr.aueb.cf.ch11;
 
 public class Student {
+   private static int studentsCount ;
    private int id;
    private String firstname;
    private String lastname;
 
-   //Default Constructor
-   public Student() {
+   static {
+      studentsCount = 0;
    }
 
-   // Overloaded Constructro
+   //Default Constructor
+   public Student() {
+      studentsCount ++;
+   }
+
+   // Overloaded Constructor
    public Student (int id, String firstname, String lastname){
       this.id = id;
       this.lastname = lastname;
       this.firstname = firstname;
+      studentsCount++;
 
    }
 
@@ -39,5 +46,9 @@ public class Student {
 
    public void setLastname(String lastname) {
       this.lastname = lastname;
+   }
+
+   public static int getStudentsCount(){
+      return studentsCount;
    }
 }
--- src/gr/aueb/cf/ch11/StudentDemo.java
+++ src/gr/aueb/cf/ch11/StudentDemo.java
@@ -3,6 +3,7 @@ package gr.aueb.cf.ch11;
 public class StudentDemo {
     public static void main(String[] args) {
         Student student = new Student();
+        Student bob = new Student(2, "Bod", "D.");
 
           student.setId(1);                                                       //student.id = 1;
           student.setFirstname("Alice");
@@ -12,5 +13,14 @@ public class StudentDemo {
         System.out.println("Id: "+ student.getId());                             //  System.out.println("Id: "+ student.id);
         System.out.println("Lastname: "+student.getLastname());                  //   System.out.println("Firstname: "+ student.firstname);
         System.out.println("Firstname: "+ student.getFirstname());             //   System.out.println("Lastname: "+student.lastname);
+
+        System.out.println("Id: "+ bob.getId());                             //  System.out.println("Id: "+ student.id);
+        System.out.println("Lastname: "+bob.getLastname());                  //   System.out.println("Firstname: "+ student.firstname);
+        System.out.println("Firstname: "+ bob.getFirstname());
+
+
+        System.out.println("Students Count: "+ Student.getStudentsCount());
     }
+
+
 }
