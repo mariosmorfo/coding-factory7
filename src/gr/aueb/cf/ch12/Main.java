d9abe217d709a2a27c5545e2b68e5e745462557f
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-01-07 20:30:52 +0200

    initial commit

--- src/gr/aueb/cf/ch12/Main.java
+++ src/gr/aueb/cf/ch12/Main.java
@@ -1,4 +1,20 @@
 package gr.aueb.cf.ch12;
 
 public class Main {
+    public static void main(String[] args) {
+        Account account = new Account(1, "GR849", "Wonderland", "Alice", "S123", 1000.0);
+        try{
+           account.deposit(900);
+            System.out.println("The deposit was successful.");
+
+            account.withDraw("R123", 400);
+
+            account.withDraw("S123", 500);
+            System.out.println("The deposit was successful");
+
+            System.out.println("Balance: "+ account.getBalance());
+        }catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
